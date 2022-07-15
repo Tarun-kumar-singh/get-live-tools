a386d0430c2a7a56605d99d79805bd25894a31e2
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-07-16 03:08:36 +0530

    n

--- src/components/Drawer/TabMenu.tsx
+++ src/components/Drawer/TabMenu.tsx
@@ -1,7 +1,6 @@
 import { useContext, useState } from "react";
-import {  Box, FormControlLabel, List, ListItem, ListItemText, Switch, Tab, Tabs } from "@mui/material";
+import {  Box, List, Tab, Tabs } from "@mui/material";
 import {useRouter} from 'next/router';
-import Link from 'next/link';
 import { Tabs as TabList } from "../../constants/GlobalConstants";
 import DarkModeContext from "../../context/DarkModeContext";
  
@@ -10,44 +9,41 @@ type Props = {
 }
 const TabMenu = (props: Props) =>{
 
-    const { onListItemClick } = props
     const router = useRouter()
     const {  handleDarkModeChange } = useContext(DarkModeContext);
 
     const [value, setValue] = useState(0);
 
-    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    const handleChange = (event: React.SyntheticEvent, newValue: number, route: string) => {
       setValue(newValue);
+      router.push(route)
     };
 
     return (
         <>
             <List>
-                {TabList.map((tab: { label: string, route: string }) => (
-                    <Link key={tab.route} href={tab.route} passHref>
-                        <Box sx={{ 
-                width: { lg: '100vw' },  
-              }}>
+              <Box sx={{ width: { lg: '100vw' } }}>
                 <Tabs
                   value={value}
-                  onChange={handleChange}
                   variant="scrollable"
                   scrollButtons
                   allowScrollButtonsMobile
                   textColor='inherit'
                   indicatorColor="secondary"
                 >
-                  <Tab label="Item One" />
-                  <Tab label="Item Two" />
-                  <Tab label="Item Three" />
-                  <Tab label="Item Four" />
-                  <Tab label="Item Five" />
-                  <Tab label="Item Six" />
-                  <Tab label="Item Seven" />
+                {TabList.map((tab: { label: string, route: string }, index: number) => (
+                    <Tab 
+                      sx={{  
+                          '&:hover': { color: 'secondary.main', opacity: 1 }, 
+                          '&.Mui-selected': { color: 'secondary.main' }
+                        }} 
+                          key={tab.label} 
+                          onClick={(e): any => handleChange(e, index, tab.route)} 
+                          label={tab.label} 
+                    />
+                ))}
                 </Tabs>
               </Box>
-                    </Link>
-                ))}
             </List>
         </>
       );
--- src/config/mui.ts
+++ src/config/mui.ts
@@ -3,6 +3,20 @@ import { PaletteSettings, TypographySettings } from "./setting";
 
 
 const theme: ThemeOptions = createTheme({
+
+    components:{
+      MuiTabs:{
+        styleOverrides:{
+          root:{
+            // background: 'red',
+            // '&.hover': { color: 'green' }
+            // '&:hover': { color: 'secondary.main', opacity: 1 }, 
+            // '& .Mui-selected': { color: 'secondary.main' }
+          }
+          } 
+          }
+        },
+
     palette: {
       ...PaletteSettings
       },
