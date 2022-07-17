a6ffd9882d3f464216f929297f083be27678a899
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-07-17 18:54:05 +0530

    a

--- src/components/text/KeywordDenisityChecker.tsx
+++ src/components/text/KeywordDenisityChecker.tsx
@@ -1,8 +1,9 @@
 import { Button, OutlinedInput, Stack } from "@mui/material";
 import { Box } from "@mui/system";
-import { useState } from "react";
-// import { removeStopwords, eng, fra } from './dist/stopword.esm.mjs'
+import { useEffect, useState } from "react";
+// import { removeStopwords, eng, fra } from 'stopword'
 
+const { removeStopwords, eng, fra } = require('stopword')
 
 type Props = {
 
@@ -10,41 +11,65 @@ type Props = {
 const ImgBlankNWhite = (props: Props) =>{
 
     const [value, setValue] = useState('')
+    const [result, setResult] = useState({} as any)
+    
+    useEffect(() =>{
+        console.log(result)
+    }, [result])
 
     const onChceck = () => {
-
+        console.log(value)
+        const newString = removeStopwords(value.split(' '))
+        console.log(getFrequency(newString))
     }
 
-    const removedStopWord = () =>{
-
+    const getFrequency = (data: Array<any>) =>{
+        console.log(data.length)
+        setResult({
+            ...result,
+            total: data.length
+        })
+        const count: any = {};
+        data.forEach((e: any) => count[e] ? count[e]++ : count[e] = 1);
+        setResult({
+            ...result,
+            frequency: count
+        })
+        return count
     }
+
 
     return(
         <>
-        <>
+            {
+                result.frequency && 
+                    <>
+                       <p>Total: {result.total}</p>
+                    </>
+            }
+           {!result.frequency && <>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                        <OutlinedInput 
+                            value={value}
+                            placeholder="Please enter text"  
+                            multiline 
+                            rows={11} 
+                            sx={{
+                                width:{
+                                    lg: '60%',
+                                    xs: '90%'
+                                },
+                                textAlign: 'center'
+                            }}
+                            onChange={(e) => setValue(e.target.value) }
+                        />
+                </div>
+            
+                <div style={{ textAlign: 'center', marginTop:'10px' }}>   
+                    <Button disabled={!(!!value)} onClick={onChceck} variant="contained">Check</Button>
+                </div> 
+            </>}
 
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-                    <OutlinedInput 
-                        value={value}
-                        placeholder="Please enter text"  
-                        multiline 
-                        rows={11} 
-                        sx={{
-                            width:{
-                                lg: '60%',
-                                xs: '90%'
-                            },
-                            textAlign: 'center'
-                        }}
-                        onChange={(e) => setValue(e.target.value) }
-                    />
-            </div>
-           
-            <div style={{ textAlign: 'center', marginTop:'10px' }}>   
-                <Button disabled={!(!!value)} onClick={onChceck} variant="contained">Check</Button>
-            </div> 
-
-        </>
         </>
     )
 }
