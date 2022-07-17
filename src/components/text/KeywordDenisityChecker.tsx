b6c234f40cb2d7f864115c7c4047690acef2965f
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-07-17 17:23:01 +0530

    bjk

--- src/components/text/KeywordDenisityChecker.tsx
+++ src/components/text/KeywordDenisityChecker.tsx
@@ -1,12 +1,45 @@
+import { Button, OutlinedInput, Stack } from "@mui/material";
+import { Box } from "@mui/system";
+import { useState } from "react";
 
 
 type Props = {
 
 }
 const ImgBlankNWhite = (props: Props) =>{
+
+    const [value, setValue] = useState('')
+
+    const onChceck = () => {
+
+    }
+
     return(
         <>
-           From Blank aNd white
+        <Box sx={{
+            display: 'flex',
+            justifyContent:'center',
+            flexDirection: 'column',
+            gap: '10px',
+            flexGrow: 1
+        }}>
+           <OutlinedInput 
+                value={value}
+                placeholder="Please enter text"  
+                multiline 
+                rows={11} 
+                sx={{
+                    width:{
+                        lg: '60%',
+                        xs: '90%'
+                    }
+                }}
+                onChange={(e) => setValue(e.target.value) }
+            />
+            <div>   
+                <Button disabled={!(!!value)} onClick={onChceck} variant="contained">Check</Button>
+            </div>
+        </Box>
         </>
     )
 }
