8323cd7a2c4e30719641ce1dd9d167833c805815
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-07-17 11:58:51 +0530

    handle events in back

--- src/constants/tools/image.ts
+++ src/constants/tools/image.ts
@@ -2,13 +2,9 @@
 
 export const ImageTools = [
     {label: 'Img to 1', name: 'ImageBlackNWhite' },
+    {label: 'Img tool - 4', name: '' },
     {label: 'Img tool - 2', name: '' },
     {label: 'Img tool - 3', name: '' },
-    {label: 'Img tool - 4', name: '' },
-    {label: 'Img tool - 5', name: '' },
-    {label: 'Img tool - 2', name: '' },
-    {label: 'Img tool - 3', name: '' },
-    {label: 'Img tool - 4', name: '' },
     {label: 'Img tool - 5', name: '' },
 
 ]
