7ef9bf63c1fb241dd48ad570bfcbb9510dc056db
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-07-18 17:12:36 +0530

    alternate case

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,12 +1,17 @@
 
 
 export const alternateCase = (text: string, upperFirst: boolean) => {
+        
+    let newText = []
     
-    let newText = text.toLowerCase().split("");
-    
+    if(upperFirst) newText = text.toLowerCase().split("");
+    else newText = text.toUpperCase().split("");
+
     for (let i = 0; i < newText.length; i += 2) {
       newText[i] = upperFirst ? newText[i].toUpperCase() : newText[i].toLowerCase()
     }
+
     return newText.join("");
+
 };
 
