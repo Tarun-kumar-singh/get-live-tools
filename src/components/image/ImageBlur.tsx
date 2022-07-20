b438009ed56cc787cf389067c42fe90a1b781d2e
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-07-20 16:42:39 +0530

    preview image

--- src/components/image/ImageBlur.tsx
+++ src/components/image/ImageBlur.tsx
@@ -1,4 +1,6 @@
 import { Button } from "@mui/material"
+import { useState } from "react"
+import Image from 'next/image'
 
 
 type Props = {
@@ -7,8 +9,24 @@ type Props = {
 const ImgeBlur = (props: Props) =>{
 
     const { onBack } = props
+    const [selectedFile, setSelectedFile] = useState()
+    const [previewImage, setPreviewImage] = useState<any>()
+
     const onClickBack = () =>{
         onBack()
+    }
+
+    const onSelectFile = (e: any) => {
+        if (!e.target.files || e.target.files.length === 0) {
+            setSelectedFile(undefined)
+            return
+        }
+console.log(e.target.files[0])
+        // I've kept this example simple by using the first image instead of multiple
+        setSelectedFile(e.target.files[0])
+
+        setPreviewImage(URL.createObjectURL(e.target.files[0]));
+
     }
     
     return(
@@ -16,13 +34,21 @@ const ImgeBlur = (props: Props) =>{
             <div style={{ marginLeft: '3%' }}>
                 <Button onClick={onClickBack} variant='outlined'>Back</Button>
             </div>
-
+         
             <div style={{ display: 'flex', justifyContent: 'center', marginTop:'10px' }}> 
                 <div style={{ display:'flex', justifyContent:'center', alignItems:'center', width: '200px', height: '250px', border: '2px black', borderStyle: 'dotted'}}>
-                    <Button size="small" variant="contained">
-                        Upload image
-                        <input hidden accept="image/*" multiple type="file" />
-                    </Button>   
+                    {!previewImage ? 
+                        <Button variant="contained" component="label">
+                            Upload image
+                            <input hidden onChange={onSelectFile} accept="image/*" multiple type="file" />
+                        </Button> : 
+                        <Image
+                            src={previewImage}
+                            alt="Picture of the author"
+                            width={200}
+                            height={250}
+                        />
+                    }  
                 </div>
             </div>
 
