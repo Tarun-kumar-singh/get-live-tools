f486325490aa8820899bf03962a0f7ff04140967
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-07-20 16:46:44 +0530

    dfwe

--- src/components/image/ImageBlur.tsx
+++ src/components/image/ImageBlur.tsx
@@ -21,7 +21,6 @@ const ImgeBlur = (props: Props) =>{
             setSelectedFile(undefined)
             return
         }
-console.log(e.target.files[0])
         // I've kept this example simple by using the first image instead of multiple
         setSelectedFile(e.target.files[0])
 
