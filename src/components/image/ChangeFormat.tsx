17bc070af840a6245b6c9fa8667d0522d8a979f6
Tarun-kumar-singh <tarunks2512@gmail.com>
2022-07-22 14:29:00 +0530

    chnage  format downlaod

--- src/components/image/ChangeFormat.tsx
+++ src/components/image/ChangeFormat.tsx
@@ -9,8 +9,8 @@ import { ObjectType } from "../../types/comman/object";
 const menuData: Array<{label: string, value: any}> = [
     { label: 'PNG', value: Jimp.MIME_PNG},
     { label: 'JPEG', value: Jimp.MIME_JPEG},
-    { label: 'GIF', value: Jimp.MIME_GIF},
-    { label: 'TIF', value: Jimp.MIME_TIFF}
+    { label: 'TIF', value: Jimp.MIME_TIFF},
+    { label: 'BMP', value: Jimp.MIME_BMP}
 ]
 
 type Props = {
@@ -20,35 +20,21 @@ const ChangeFormat = (props: Props) =>{
 
     const { onBack } = props
 
-
-
-    const [selectedFile, setSelectedFile] = useState<Blob | MediaSource | undefined>()
-    const [previewImage, setPreviewImage] = useState<string>('')
-    const [selectedImageBase64, setSelectedImageBase64] = useState<string>('')
-
     const [displayLoader, setDisplayLoader] = useState(false)
     const [imageType, setImageType] = useState('')
 
     const [selectedImageUrl, setSelectedImageUrl] = useState('')
-
     const [changeFormatValue, setChangeFormatValue] = useState('')
-
-    useEffect(() =>{
-        console.log(changeFormatValue)
-      }, [changeFormatValue])
 
     const onSelectFile = async(e: any) => {
         setDisplayLoader(true)
         if (!e.target.files || e.target.files.length === 0) {
-            setSelectedFile(undefined)
             setDisplayLoader(false)
             return
         }
     
-        setSelectedFile(e.target.files[0])
         setImageType((e.target.files[0].type).split('/')[1])
         const selectedImageURL = URL.createObjectURL(e.target.files[0])
-        console.log(Jimp.MIME_JPEG)
         setSelectedImageUrl(selectedImageURL)
         setDisplayLoader(false)
  
@@ -61,10 +47,9 @@ const ChangeFormat = (props: Props) =>{
     const changeFormatType = async(type: string) =>{
         setDisplayLoader(true)
         const jimpRead = await Jimp.read(selectedImageUrl)
-        
+        console.log(changeFormatValue)
         jimpRead.getBase64(changeFormatValue, (err, src) =>{
-            setSelectedImageBase64(src)
-            setPreviewImage(src)
+            console.log(err)
             setDisplayLoader(false)
             downloadImageFromBase64(src, `Image.${type.split('/')[1]}`)
         })
@@ -72,7 +57,7 @@ const ChangeFormat = (props: Props) =>{
     }
 
     const downloadImageFromBase64 = (base64Data: string, name: string) =>{
-        console.log(name)
+       console.log(name)
         var a = document.createElement("a"); //Create <a>
         a.href = base64Data; //Image Base64 Goes here
         a.download = name; //File name Here
